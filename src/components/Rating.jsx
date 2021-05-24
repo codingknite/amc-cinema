cd6e0ea2f1b200d79658e8605ec9639ccac65941
mojpm <itsjoelpm@gmail.com>
2021-05-25 01:47:55 +0300

    add movie rating component

---
+++ src/components/Rating.jsx
@@ -0,0 +1,24 @@
+import Star from 'react-rating';
+import PropTypes from 'prop-types';
+import * as AiIcons from 'react-icons/ai';
+
+import { themes } from '../styles/themes';
+
+const Rating = ({ initialRating }) => {
+  return (
+    <Star
+      emptySymbol={
+        <AiIcons.AiOutlineStar size="22" color={themes.colors.white} />
+      }
+      fullSymbol={<AiIcons.AiFillStar size="22" color={themes.colors.orange} />}
+      initialRating={initialRating}
+      readonly
+    />
+  );
+};
+
+Rating.propTypes = {
+  initialRating: PropTypes.number.isRequired,
+};
+
+export default Rating;
