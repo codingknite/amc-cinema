890997dd045497fb6d07f12f26c83ce2fe063f51
mojpm <itsjoelpm@gmail.com>
2021-05-25 01:48:54 +0300

    add discover genres and categories components

---
+++ src/components/Discover/Categories/index.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import * as Styles from './styles';
+import generateSlug from '../../../utils/generateSlug';
+import MovieCards from '../../MovieCards/index';
+import { useFavorite } from '../../../context/useFavorites';
+
+const DiscoverCategories = ({ category, data, loading }) => {
+  const { favorites, dispatchFavorites } = useFavorite();
+  const displayData = data.slice(0, 12);
+
+  console.log(displayData);
+  console.log(favorites);
+
+  return (
+    <>
+      <Styles.Header>{category}</Styles.Header>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <Styles.Wrapper>
+          <MovieCards
+            topMargin="6.5rem"
+            data={displayData}
+            favorites={favorites}
+            dispatchFavorites={dispatchFavorites}
+          />
+          <Link to={`/discover/${generateSlug(category)}`}>See All</Link>
+        </Styles.Wrapper>
+      )}
+    </>
+  );
+};
+
+DiscoverCategories.propTypes = {
+  category: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default DiscoverCategories;
