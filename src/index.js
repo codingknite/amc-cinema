8973151017a3f7a739ff4fd1c47493088444527e
mojpm <itsjoelpm@gmail.com>
2021-05-25 01:44:33 +0300

    add favorites provider

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { FavoritesProvider } from './context/useFavorites';
+
 
 const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-    </QueryClientProvider>
+    <FavoritesProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <App />
+        </Router>
+      </QueryClientProvider>
+    </FavoritesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
