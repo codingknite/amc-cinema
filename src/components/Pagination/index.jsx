9416d5dc1b27698ca1fb6775c6717384ec7a1a48
mojpm <itsjoelpm@gmail.com>
2021-05-26 17:37:31 +0300

    add pagination component

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,67 @@
+import PropTypes from 'prop-types';
+import { scroller } from 'react-scroll';
+import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr';
+
+import * as Styles from './styles';
+
+const Pagination = ({ pageState, dispatch, page, pages }) => {
+  const scrollTo = () => {
+    return scroller.scrollTo('scrollelement', {
+      duration: 1500,
+      smooth: 'easeInOutQuard',
+      offset: -50,
+    });
+  };
+
+  return (
+    <>
+      {pages === 1 ? null : pageState === 1 ? (
+        <Styles.NextButton
+          onClick={() => {
+            dispatch({ type: 'next' });
+            scrollTo();
+          }}
+        >
+          Page {page + 1} <GrLinkNext color="white" />
+        </Styles.NextButton>
+      ) : pageState === pages ? (
+        <Styles.PrevButton
+          onClick={() => {
+            dispatch({ type: 'prev' });
+            scrollTo();
+          }}
+        >
+          <GrLinkPrevious /> Page {page - 1}
+        </Styles.PrevButton>
+      ) : (
+        <>
+          <Styles.PrevButton
+            onClick={() => {
+              dispatch({ type: 'prev' });
+              scrollTo();
+            }}
+          >
+            <GrLinkPrevious color={'white'} /> Page {pageState - 1}
+          </Styles.PrevButton>
+          <Styles.NextButton
+            onClick={() => {
+              dispatch({ type: 'next' });
+              scrollTo();
+            }}
+          >
+            Page {pageState + 1} <GrLinkNext color="white" />
+          </Styles.NextButton>
+        </>
+      )}
+    </>
+  );
+};
+
+Pagination.propTypes = {
+  pageState: PropTypes.number.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  pages: PropTypes.number.isRequired,
+};
+
+export default Pagination;
