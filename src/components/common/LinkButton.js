83914d6f10e566b80bf4b016aa98eae7f4581993
mojpm <itsjoelpm@gmail.com>
2021-05-26 08:52:57 +0300

    complete adding styled link button

---
+++ src/components/common/LinkButton.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { themes } from 'styles/themes';
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: ${themes.colors.white};
+
+`;
+
+export const StyledLinkButton = styled(Link)`
+  border: none;
+  width: ${(props) => props.width ? props.width : '9rem'};
+  padding: 0.8rem;
+  margin-right: 1rem;
+  margin-top: 1rem;
+  border-radius: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  color: ${themes.colors.white};
+  background-color: ${themes.colors.heartRed};
+  align-self: ${(props) => props.flexEnd ? 'flex-end' : 'flex-start'};
+  margin-bottom: ${(props) => props.marginBottom ? props.marginBottom : '0'};
+`;
