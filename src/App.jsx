c0d3f5543a1762c6882085fd474be4b0cc6f5b2f
mojpm <itsjoelpm@gmail.com>
2021-05-27 18:04:53 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 
 import Search from 'pages/Search';
+import Discover from 'pages/Discover';
 import Bookings from 'pages/Bookings';
 import Homepage from 'pages/Homepage';
+import { themes } from 'styles/themes';
+import BookMovie from 'pages/BookMovie';
 import Biography from 'pages/Biography';
 import Favorites from 'pages/Favorites';
-import { themes } from 'styles/themes';
+import MovieDetails from 'pages/MovieDetails';
+import DiscoverGenres from 'pages/DiscoverGenres';
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -29,9 +33,13 @@ const App = () => {
       <Routes>
         <Route path="/" exact element={<Homepage />} />
         <Route path="/favorites" element={<Favorites />} />
-        <Route path="/my-bookings" element={<Bookings />} />
-        <Route path="/biography/:personId" element={<Biography />} />
         <Route path="/search" exact element={<Search />} />
+        <Route path="/my-bookings" element={<Bookings />} />
+        <Route path="movie/:movieId" element={<MovieDetails />} />
+        <Route path={'/booking/:movieId'} element={<BookMovie />} />
+        <Route path="/biography/:personId" element={<Biography />} />
+        <Route path={'/discover/:category'} element={<Discover />} />
+        <Route path={'/genres/:genre'} element={<DiscoverGenres />} />
       </Routes>
     </>
   );
