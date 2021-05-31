33473920e35cf322704956dce559e83c081bc721
mojpm <itsjoelpm@gmail.com>
2021-05-31 08:58:38 +0300

    update cast config settings

--- src/components/Movie/Cast.jsx
+++ src/components/Movie/Cast.jsx
@@ -6,18 +6,25 @@ import 'slick-carousel/slick/slick.css';
 import * as config from 'utils/config';
 import CastItem from 'components/Movie/CastItem';
 
-const MovieCast = ({ cast }) => (
-  <Slider {...config.castSettings}>
-    {cast.map((cast) => (
-      <CastItem
-        name={cast.name}
-        personId={cast.id}
-        castId={cast.cast_id}
-        profilePath={cast.profile_path}
-      />
-    ))}
-  </Slider>
-);
+const MovieCast = ({ cast }) => {
+  const castSettings = {
+    ...config.castSettings,
+    slidesToShow: cast.length < 12 ? cast.length : 12,
+  };
+
+  return (
+    <Slider {...castSettings}>
+      {cast.map((cast) => (
+        <CastItem
+          name={cast.name}
+          personId={cast.id}
+          castId={cast.cast_id}
+          profilePath={cast.profile_path}
+        />
+      ))}
+    </Slider>
+  );
+};
 
 MovieCast.propTypes = {
   cast: PropTypes.array.isRequired,
