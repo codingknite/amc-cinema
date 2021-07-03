beeeb487f155b2da72a0dc874ce1d1529f5b1ea7
mojpm <itsjoelpm@gmail.com>
2021-07-04 00:34:55 +0300

    update style rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,42 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    'jest/globals': true
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    ecmaVersion: 2018,
-    sourceType: 'module'
+    'ecmaVersion': 12,
+    'sourceType': 'module'
   },
-  plugins: ['react', 'jest'],
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { 'before': true, 'after': true }],
-    'no-console': 0,
-    'react/prop-types': 0
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
   }
 };
