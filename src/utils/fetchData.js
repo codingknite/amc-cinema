713cc2e46403844c108be7e0b5afb6c089c08fc2
mojpm <itsjoelpm@gmail.com>
2021-07-05 00:37:53 +0300

    use async/await

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,14 +1,16 @@
-export async function fetchData(url) {
+import axios from 'axios';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+async function fetchData(url) {
   try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      if (data) return data;
-    } else {
-      throw response;
-    }
+    const response = await axios.get(url);
+    const data = response.data;
+    if (data) return data;
+    throw response;
   } catch (error) {
-    console.log('WE HAVE AN ERROR ===>', error.status);
-    return error.status;
+    console.log('AN ERROR OCCURED ->', error);
+    return error;
   }
 }
+
+export default fetchData;
