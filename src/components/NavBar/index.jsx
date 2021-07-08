d384fa25ec8bfa55fea5b03242d4dd5bb3a88cee
mojpm <itsjoelpm@gmail.com>
2021-07-08 15:20:44 +0300

    redefine nav bar routes

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,28 +1,19 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaChevronDown } from 'react-icons/fa';
-import { BsSearch, BsCalendar } from 'react-icons/bs';
+import * as BsIcons from 'react-icons/bs';
 
-import { useState } from 'react';
 import * as Styles from './styles';
-import generateSlug from 'utils/generateSlug';
-import { useFavorite } from 'context/useFavorites';
-import { discover, genres } from 'data/categories';
+import Logo from 'assets/logo.png';
+
 const NavBar = () => {
   const navigate = useNavigate();
-  const { favorites } = useFavorite();
-  const totalFavorites = favorites.length;
   const [isClicked, setIsClicked] = useState(false);
 
   const handleClick = () => setIsClicked(!isClicked);
 
   return (
     <Styles.Nav>
-      <Styles.Logo
-        src="https://fontmeme.com/permalink/210601/cdb13a4e4aeb6a1a64070ce3253d854a.png"
-        alt="logo"
-        onClick={() => navigate('/')}
-      />
+      <Styles.Logo src={Logo} alt="logo" onClick={() => navigate('/')} />
       <Styles.Hamburger
         onClick={handleClick}
         className={isClicked ? 'toggle' : ''}
@@ -42,70 +33,52 @@ const NavBar = () => {
             Home
           </Styles.StyledNavLink>
         </li>
-        <Styles.DropdownList>
-          <Styles.Dropdown>
-            <p>Discover</p>
-            <FaChevronDown style={{ marginLeft: '0.5rem' }} />
-          </Styles.Dropdown>
-          <div className="dropdown-content">
-            {discover.map((genre, index) => (
-              <Styles.DropdownNav
-                key={index}
-                to={`/discover/${generateSlug(genre)}`}
-                activeClassName="active-genre"
-              >
-                {genre}
-              </Styles.DropdownNav>
-            ))}
-          </div>
-        </Styles.DropdownList>
-        <Styles.DropdownList>
-          <Styles.Dropdown>
-            <p>Genres</p>
-            <FaChevronDown style={{ marginLeft: '0.5rem' }} />
-          </Styles.Dropdown>
-          <div className="dropdown2">
-            {genres.map((genre) => (
-              <Styles.DropdownNav
-                drop2
-                key={genre.id}
-                to={`/genres/${generateSlug(genre.name)}`}
-                activeClassName="active-genre"
-              >
-                {genre.name}
-              </Styles.DropdownNav>
-            ))}
-          </div>
-        </Styles.DropdownList>
+        <li>
+          <Styles.StyledNavLink
+            to="/movies"
+            className="font fontSize"
+            activeClassName="active"
+          >
+            Movies
+          </Styles.StyledNavLink>
+        </li>
+        <li>
+          <Styles.StyledNavLink
+            to="/movies"
+            className="font fontSize"
+            activeClassName="active"
+          >
+            Series
+          </Styles.StyledNavLink>
+        </li>
         <li>
           <Styles.StyledNavLink
             to="/favorites"
             className="font fontSize"
             activeClassName="active"
           >
-            {`Favorites ( ${totalFavorites} )`}
+            <BsIcons.BsBookmarkFill style={{ marginRight: '0.5rem' }} />
+            Favorites
           </Styles.StyledNavLink>
         </li>
         <li>
           <Styles.StyledNavLink
-            flexDisplay={true}
             to="/my-bookings"
             className="font fontSize"
-            activeClassName="active-bookmark"
+            activeClassName="active"
           >
-            <BsCalendar style={{ marginRight: '0.5rem' }} />
-            <p>My Bookings</p>
+            <BsIcons.BsCalendar style={{ marginRight: '0.5rem' }} />
+            Bookings
           </Styles.StyledNavLink>
         </li>
         <li>
           <Styles.StyledNavLink
-            flexDisplay={true}
             to="/search"
             className="font fontSize"
-            activeClassName="active-bookmark"
+            activeClassName="active"
           >
-            <BsSearch size="18" style={{ marginRight: '0.5rem' }} />
-            <p>Search</p>
+            <BsIcons.BsSearch size="18" style={{ marginRight: '0.5rem' }} />
+            Search
           </Styles.StyledNavLink>
         </li>
       </ul>
