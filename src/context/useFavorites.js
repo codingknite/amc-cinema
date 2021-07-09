842b73e19f623025fcae10fd821cd2a41e2dd562
mojpm <itsjoelpm@gmail.com>
2021-07-10 00:44:46 +0300

    major bug fixes

--- src/context/useFavorites.js
+++ src/context/useFavorites.js
@@ -6,44 +6,44 @@ const FavoriteContext = React.createContext(null);
 
 let initialFavorites;
 try {
-    initialFavorites = JSON.parse(localStorage.getItem('favorite')) ?? [];
+  initialFavorites = JSON.parse(localStorage.getItem('favorite')) ?? [];
 } catch {
-    initialFavorites = [];
-    console.error('Favorites could not be parsed');
+  initialFavorites = [];
+  console.error('Favorites could not be parsed');
 }
 
 export function FavoritesProvider({ children }) {
-    const [favorites, dispatchFavorites] = useReducer(
-        favoritesReducer,
-        initialFavorites
-    );
+  const [favorites, dispatchFavorites] = useReducer(
+    favoritesReducer,
+    initialFavorites,
+  );
 
-    useEffect(() => localStorage.setItem('favorite', JSON.stringify(favorites)), [
-        favorites,
-    ]);
+  useEffect(() => localStorage.setItem('favorite', JSON.stringify(favorites)), [
+    favorites,
+  ]);
 
-    const contextValue = {
-        favorites,
-        dispatchFavorites,
-    };
+  const contextValue = {
+    favorites,
+    dispatchFavorites,
+  };
 
-    return (
-        <FavoriteContext.Provider value={contextValue}>
-            {children}
-        </FavoriteContext.Provider>
-    );
+  return (
+    <FavoriteContext.Provider value={contextValue}>
+      {children}
+    </FavoriteContext.Provider>
+  );
 }
 
 export function useFavorite() {
-    const context = useContext(FavoriteContext);
-    if (!context) {
-        throw new Error(
-            'useFavorite must be used within a FavoriteProvider. Wrap a parent component in <FavoriteProvider> to fix this error.'
-        );
-    }
-    return context;
+  const context = useContext(FavoriteContext);
+  if (!context) {
+    throw new Error(
+      'useFavorite must be used within a FavoriteProvider. Wrap a parent component in <FavoriteProvider> to fix this error.',
+    );
+  }
+  return context;
 }
 
 FavoritesProvider.propTypes = {
-    children: PropTypes.any.isRequired
+  children: PropTypes.any.isRequired,
 };
